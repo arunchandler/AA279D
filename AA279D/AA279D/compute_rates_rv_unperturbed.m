d0fae6b7456bafad43dfafa43ed3c7bab0462d96
Arun Chandler <100224289+arunchandler@users.noreply.github.com>
2025-04-08 18:23:58 -0700

    Finished questions, format

---
+++ AA279D/AA279D/compute_rates_rv_unperturbed.m
@@ -0,0 +1,12 @@
+% rates function for numerical integration for propagating rv state
+% unperturbed
+
+function [rv_dot] = compute_rates_rv_unperturbed(t, rv)
+
+    mu = 3.986004418e14; % (m^3/s^2)
+    
+    rv_dot = zeros(6,1);
+    rv_dot(1:3) = rv(4:6);
+    rv_dot(4:6) = -mu .* rv(1:3) / norm(rv(1:3))^3;
+
+end
